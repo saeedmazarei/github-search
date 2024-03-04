aa14fcbe8b74959e6a93779d4378f381b77d0f4a
Mazare <lsaeed34@gmail.com>
2024-03-04 22:59:28 +0330

    Add search bar UI

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,7 +1,15 @@
+import Button from '../../components/shared/button/Button'
 import styles from './Index.module.scss'
 
 function Home() {
-    return <div className={styles.home}>Home</div>
+    return (
+        <div className={styles.home}>
+            <div className={styles.searchBar}>
+                <input type="text" placeholder="Search Users..." />
+                <Button className={styles.searchButton}>Submit</Button>
+            </div>
+        </div>
+    )
 }
 
 export default Home
