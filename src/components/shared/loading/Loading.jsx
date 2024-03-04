aba3d367e90b24be67ed3a8369070798f45b43ae
Mazare <lsaeed34@gmail.com>
2024-03-04 20:09:10 +0330

    Add loading component

---
+++ src/components/shared/loading/Loading.jsx
@@ -0,0 +1,9 @@
+import Card from "../card/Card"
+import loadingImage from '/assets/images/loading.svg'
+import styles from './Loading.module.scss'
+
+function Loading() {
+    return <Card className={styles['loading-container']}><img src={loadingImage} alt="Loading" /></Card>
+}
+
+export default Loading
