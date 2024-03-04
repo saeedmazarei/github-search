891b0796a5ff8802f252315b3e3091e88849d689
Mazare <lsaeed34@gmail.com>
2024-03-04 17:43:20 +0330

    Add routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { Suspense, lazy } from 'react'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import PathConstants from './routes/pathConstants'
+const Home = lazy(() => import('./pages/home/Index'))
+const Detail = lazy(() => import('./pages/detail/Index'))
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            children: [
+                {
+                    path: PathConstants.HOME,
+                    element: <Home />,
+                },
+                {
+                    path: PathConstants.REPODETAILS,
+                    element: <Detail />,
+                },
+            ],
+        },
+    ])
 
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <Suspense>
+            <RouterProvider router={router} />
+        </Suspense>
+    )
 }
 
 export default App
