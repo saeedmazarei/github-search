561671aa299c3caeb321afa2295fae5ccad158d9
Mazare <lsaeed34@gmail.com>
2024-03-05 21:50:08 +0330

    Add api call to get repo detail

--- src/pages/detail/Index.jsx
+++ src/pages/detail/Index.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from 'react'
 
 import { useParams } from 'react-router-dom'
 
-import { getUserDetail } from '../../services/apis'
+import { getRepo, getUserDetail } from '../../services/apis'
 import Loading from '../../components/shared/loading/Loading'
 import BackButton from './back-button/BackButton'
 import styles from './Index.module.scss'
 import ProfileAndBio from './profile-bio/ProfileAndBio'
 import FollowerFolowing from './follower-following/FollowerFolowing'
+import Projects from './projects/Projects'
 
 function Detail() {
     const [userDetailData, setUserDetailData] = useState({})
+    const [repoDetails, setRepoDetails] = useState([])
     const [loading, setLoading] = useState(false)
     const { owner } = useParams()
 
@@ -18,6 +20,9 @@ function Detail() {
         const getUserDetailFunc = async () => {
             setLoading(true)
             const res = await getUserDetail(owner)
+            const response = await getRepo(owner)
+            setRepoDetails(response.data)
+            console.log(response.data)
             setUserDetailData(res.data)
             setLoading(false)
         }
@@ -27,11 +32,20 @@ function Detail() {
     return (
         <>
             {loading && <Loading />}
-            {!loading &&<div className={styles.detail}>
-                <BackButton hireable={userDetailData.hireable} />
-                <ProfileAndBio DetailData={userDetailData} />
-                <FollowerFolowing userDetails={userDetailData} />
-            </div>}
+            {!loading && (
+                <div className={styles.detail}>
+                    <BackButton hireable={userDetailData.hireable} />
+                    <ProfileAndBio DetailData={userDetailData} />
+                    <FollowerFolowing userDetails={userDetailData} />
+                    {repoDetails.map((repo) => (
+                        <Projects
+                            key={repo.id}
+                            projectName={repo.name}
+                            projectLink={repo.html_url}
+                        />
+                    ))}
+                </div>
+            )}
         </>
     )
 }
--- src/services/apis.js
+++ src/services/apis.js
@@ -3,3 +3,5 @@ import api from './config'
 export const getUser = (query) => api.get(`search/users?q=${query}`)
 
 export const getUserDetail = (userName) => api.get(`users/${userName}`)
+
+export const getRepo = (userName) => api.get(`users/${userName}/repos`)
