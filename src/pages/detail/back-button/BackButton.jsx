098e3b05864ec1e477b4fb3b87396402e48d6801
Mazare <lsaeed34@gmail.com>
2024-03-05 16:29:35 +0330

    Add icon for hireable

--- src/pages/detail/back-button/BackButton.jsx
+++ src/pages/detail/back-button/BackButton.jsx
@@ -1,14 +1,23 @@
+import PropTypes from 'prop-types'
+
 import { Link } from 'react-router-dom'
+import { FaCheck } from "react-icons/fa"
+import { RxCross2 } from "react-icons/rx"
+
 import Card from '../../../components/shared/card/Card'
 import styles from './BackButton.module.scss'
 
-function BackButton() {
+function BackButton({ hireable }) {
     return <div className={styles.backButtonContainer}>
         <Card className={styles.backButton}>
             <Link to={"/"}>Back To Search</Link>
         </Card>
-        <span>hierable: {}</span>
+        <span>hierable: {hireable ? <FaCheck style={{ color: 'green' }} /> : <RxCross2 style={{ color: 'red' }} />}</span>
     </div>
+}
+
+BackButton.propTypes = {
+    hireable: PropTypes.bool
 }
 
 export default BackButton
